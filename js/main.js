64b8e5d0af2031c815e1758e6063f815574a8bda
lifeofwale <waledebowale@gmail.com>
2019-03-06 10:46:10 +0100

    @conference

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
 
-    $(document).ready(function(){
-	$('a.scroll-link').click(function(e){
-		e.preventDefault();
-		$id = $(this).attr('href');
-		$('html, body').animate({
-			scrollTop: $($id).offset().top -100
-		}, 750);
-	});	
-});
   
